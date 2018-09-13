ef66f362db5e0cc822d84bd6d0d6a914c630a364
pedrodiamel <pdmf@cin.ufpe.br>
2018-09-13 20:49:21 +0000

    update test

--- test/test_synthetic_transforms.py
+++ test/test_synthetic_transforms.py
@@ -191,7 +191,7 @@ def test_image_transform():
     plt.show()
 
 
-test_image_transform()
+#test_image_transform()
 
 
 def test_transform():
@@ -216,6 +216,7 @@ def test_transform():
     tranform_image_and_mask_performs(data, name, num_transform, bsave, bshow, bgrid)
 
 
+test_transform()
 
 
 
